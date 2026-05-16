bd6b4c75c80017bb8bf021ae3ac29c2942934350
Andrej Varszegi <andrejko45@gmail.com>
2026-05-16 20:26:28 +0200

    Docker is running

---
+++ backend/src/main/java/com/danto/ShootApp/ShootAppApplication.java
@@ -0,0 +1,13 @@
+package com.danto.ShootApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShootAppApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/danto/ShootApp/ShootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danto.ShootApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
